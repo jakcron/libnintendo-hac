89ec6fbdf67cde8ae0590578fbd014213353739b
Jack <jakcron.dev@gmail.com>
2020-02-26 16:36:39 +0800

    Rename ApplicationControlPropertyUtils to ApplicationControlPropertyUtil

---
+++ src/ApplicationControlPropertyUtil.cpp
@@ -0,0 +1,11 @@
+#include <nn/hac/ApplicationControlPropertyUtil.h>
+
+bool nn::hac::ApplicationControlPropertyUtil::validateSaveDataSizeMax(int64_t size, int64_t alignment)
+{
+	return (alignment != 0 && (size & (alignment - 1)) != 0) == false;
+}
+
+bool nn::hac::ApplicationControlPropertyUtil::validateSaveDataSize(int64_t size)
+{
+	return (size & 0x3fff) == 0;
+}
