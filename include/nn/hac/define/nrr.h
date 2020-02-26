e10c24dbf25cdb7abf83519bd328f9f60fcc55b0
Jack <jakcron.dev@gmail.com>
2020-02-26 14:15:47 +0800

    Update NRR specs.

--- include/nn/hac/define/nrr.h
+++ include/nn/hac/define/nrr.h
@@ -10,6 +10,12 @@ namespace hac
 	namespace nrr
 	{
 		static const uint32_t kNrrStructMagic = _MAKE_STRUCT_MAGIC_U32("NRR0");
+
+		enum NrrKind
+		{
+			User = 0,
+			JitPlugin = 1
+		};
 	}
 
 #pragma pack(push,1)
@@ -24,16 +30,18 @@ namespace hac
 	struct sNrrHeader
 	{
 		le_uint32_t st_magic;
-		byte_t reserved_0[28];
+		le_uint32_t signature_key_generationn;
+		byte_t reserved_0[24];
 		sNrrCertificate certificate;
 		byte_t nrr_body_signature[fnd::rsa::kRsa2048Size];
 		le_uint64_t application_id;
 		le_uint32_t size;
-		byte_t reserved_1[4];
+		le_uint32_t nrr_kind;
 		le_uint32_t hash_offset;
 		le_uint32_t hash_count;
-		byte_t reserved_2[8];
+		byte_t reserved_1[8];
 	};
+
 #pragma pack(pop)
 }
 }
