215255f202b382e4108b9a7fad242e0cb3e838ae
Jack <jakcron.dev@gmail.com>
2020-02-27 19:01:16 +0800

    Add AsString() methods to GameCardUtil

--- include/nn/hac/GameCardUtil.h
+++ include/nn/hac/GameCardUtil.h
@@ -11,6 +11,13 @@ namespace hac
 		static inline uint64_t blockToAddr(uint32_t block) { return ((uint64_t)block) << 9; }
 		static void getXciHeaderAesIv(const nn::hac::sGcHeader* hdr, byte_t* iv);
 		static void decryptXciHeader(nn::hac::sGcHeader* hdr, const byte_t* key);
+
+		static std::string getKekIndexAsString(nn::hac::gc::KekIndex val);
+		static std::string getRomSizeAsString(nn::hac::gc::RomSize val);
+		static std::string getHeaderFlagsAsString(nn::hac::gc::HeaderFlags val);
+		static std::string getCardFwVersionDescriptionAsString(nn::hac::gc::FwVersion val);
+		static std::string getCardClockRateAsString(nn::hac::gc::CardClockRate val);
+		static std::string getCompatibilityTypeAsString(nn::hac::gc::CompatibilityType val);
 	};
 }
 }
