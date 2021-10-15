992350cb940ced51a002f956f90a48001539cd91
jakcron <jakcron.dev@gmail.com>
2021-10-15 15:54:44 +0800

    Misc change to sHierarchicalSha256Header

--- include/nn/hac/define/hierarchicalsha256.h
+++ include/nn/hac/define/hierarchicalsha256.h
@@ -14,14 +14,16 @@ namespace hac
 #pragma pack(push,1)
 	struct sHierarchicalSha256Header
 	{
-		detail::sha256_hash_t master_hash;
-		tc::bn::le32<uint32_t> hash_block_size;
-		tc::bn::le32<uint32_t> layer_num;
 		struct sLayer
 		{
 			tc::bn::le64<int64_t> offset;
 			tc::bn::le64<int64_t> size;
-		} layer[hierarchicalsha256::kMaxLayerNum];
+		};
+
+		detail::sha256_hash_t master_hash;
+		tc::bn::le32<uint32_t> hash_block_size;
+		tc::bn::le32<uint32_t> layer_num;
+		std::array<sLayer, hierarchicalsha256::kMaxLayerNum> layer;
 	};
 	static_assert(sizeof(sHierarchicalSha256Header) == 0x48, "sHierarchicalSha256Header size.");
 #pragma pack(pop)
